379252e8dd62559889efe40fc264a122072711aa
KckOnur <onur.unique8@gmail.com>
2023-02-11 19:21:45 +0300

    Enemy Script Added

---
+++ UdemyProject2/Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UdemyProject2.Movements;
+using UnityEngine;
+
+namespace UdemyProject2.Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+        VerticalMover _verticalMover;
+        float _currenLifeTime=0f;
+
+        [SerializeField] float _moveSpeed=10f;
+        [SerializeField] float _maxLifeTime=10f;
+
+
+        public float MoveSpeed => _moveSpeed;
+
+        private void Awake()
+        {
+            _verticalMover = new VerticalMover(this);
+        }
+
+        private void Update()
+        {
+            _currenLifeTime += Time.deltaTime;
+            if(_currenLifeTime>_maxLifeTime)
+            {
+                _currenLifeTime = 0f;
+                KillYourSelf();
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            _verticalMover.FixedTick();
+        }
+
+
+        void KillYourSelf()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
+
